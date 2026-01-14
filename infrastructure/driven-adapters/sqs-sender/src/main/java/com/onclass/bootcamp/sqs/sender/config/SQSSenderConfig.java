5413d9a49d65fbf52023bc64a7a3936eacc8f0a2
Yeferson Fernandez <yeferson1500@gmail.com>
2026-01-13 21:11:19 -0500

    feat(bootcamp): implement SQS sender adapter for report events

---
+++ infrastructure/driven-adapters/sqs-sender/src/main/java/com/onclass/bootcamp/sqs/sender/config/SQSSenderConfig.java
@@ -0,0 +1,38 @@
+package com.onclass.bootcamp.sqs.sender.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+@ConditionalOnMissingBean(SqsAsyncClient.class)
+public class SQSSenderConfig {
+
+    @Bean
+    public SqsAsyncClient configSqs(SQSSenderProperties properties) {
+        return SqsAsyncClient.builder()
+                .region(Region.of(properties.region()))
+                .credentialsProvider(getProviderChain())
+                .build();
+    }
+
+    private AwsCredentialsProviderChain getProviderChain() {
+        return AwsCredentialsProviderChain.builder()
+                .addCredentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .addCredentialsProvider(SystemPropertyCredentialsProvider.create())
+                .addCredentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .addCredentialsProvider(ProfileCredentialsProvider.create())
+                .addCredentialsProvider(ContainerCredentialsProvider.builder().build())
+                .addCredentialsProvider(InstanceProfileCredentialsProvider.create())
+                .build();
+    }
+}
